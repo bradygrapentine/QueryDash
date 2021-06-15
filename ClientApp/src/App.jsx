dab0f71fcc607da89f9e50efbaba9244658f6a38
bradygrapentine <brady.grapentine@gmail.com>
2021-06-14 23:13:56 -0400

    added basic HTML and CSS for Dash Page

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -3,6 +3,449 @@ import './custom.scss'
 
 export function App() {
   return (
-    <div className="d-flex justify-content-center display-2">Hello, World!</div>
+    <>
+      <main className="main">
+        <header>
+          {' '}
+          <h2>QueryDash</h2>
+        </header>
+        <form className="search">
+          <input className="search" type="text" placeholder="Type Here" />
+        </form>
+        <section className="menu">
+          Menu
+          <div className="menuContent">
+            <button>Open</button>
+            <button>Archive</button>
+            <button>History</button>
+            <button>Dash Settings</button>
+            <button className="settings">Account</button>
+            <button>Home</button>
+          </div>
+        </section>
+        <div className="container">
+          <h3 className="header">Dash Header</h3>
+          <div className="display">
+            <div className="panel">
+              <h6 className="header">Endpoint Head</h6>{' '}
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+            </div>
+            <div className="panel">
+              <h6 className="header">Endpoint Head</h6>{' '}
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+            </div>
+            <div className="panel">
+              <h6 className="header">Endpoint Head</h6>{' '}
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+            </div>
+            <div className="panel">
+              <h6 className="header">Endpoint Head</h6>{' '}
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+            </div>
+            <div className="panel">
+              <h6 className="header">Endpoint Head</h6>{' '}
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+            </div>
+            <div className="panel">
+              <h6 className="header">Endpoint Head</h6>{' '}
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+            </div>
+            <div className="panel">
+              <h6 className="header">Endpoint Head</h6>{' '}
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+            </div>
+            <div className="panel">
+              <h6 className="header">Endpoint Head</h6>{' '}
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+              <div className="link">
+                <button></button>
+                <a href="google.com">
+                  {' '}
+                  <p>Wikipedia</p>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer>FOOT</footer>
+    </>
   )
 }
