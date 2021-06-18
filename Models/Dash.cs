d1a93fffdcdcc6aa53dd82f69ca13708a48f2b14
bradygrapentine <brady.grapentine@gmail.com>
2021-06-17 21:19:37 -0400

    added models as well as some speculation about redundancy scaling and future imporvements in the comments

--- Models/Dash.cs
+++ Models/Dash.cs
@@ -53,3 +53,23 @@ namespace QueryDash.Models
 // public List<DashQuery> DashQueryHistory { get; set; }
 // public List<OpenedLink> OpenHistory { get; set; }
 // public List<string> DisplayPreferences { get; set; }
+
+
+
+// using System;
+// using System.Collections.Generic;
+// namespace QueryDash.Models
+// {
+//     public class Panel
+//     {
+//         public int Id { get; set; }
+//         public int DashId { get; set; }
+//         public string EndpointUrl { get; set; }
+//         public string EndpointName { get; set; }
+//         public string EndpointLogoUrl { get; set; }
+// public List<string> endpointResults {get; set;}
+
+//         // can this be completely nested in the Dash Object?
+//         // - let's try - it may solve the redundancy issue entirely and simplify the database and ERD immensely simpler
+//     }
+// }
