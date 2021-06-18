ccfbd9f019a2f9c362106a11c124716acefe47d4
bradygrapentine <brady.grapentine@gmail.com>
2021-06-18 01:43:41 -0400

    added models as well as some speculation about redundancy scaling and future imporvements in the comments

---
+++ Models/Panel.cs
@@ -0,0 +1,13 @@
+// using System;
+// using System.Collections.Generic;
+// namespace QueryDash.Models
+// {
+//     public class Panel
+//     {
+//         public int Id { get; set; }
+//         // public int UserId { get; set; }                      // Presets could have null UserId
+//         public string Endpoint { get; set; }
+//         public string EndpointName { get; set; }
+//         public string EndpointLogo { get; set; }
+//     }
+// }
